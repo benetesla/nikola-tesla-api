d469451b44233967294b2eecce7869aa069c0ca0
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-21 15:38:44 -0300

    mudando o nome da nossa aplicacao  de = api-tesla para client

--- src/components/Patentes/Patentes.jsx
+++ src/components/Patentes/Patentes.jsx
@@ -2,14 +2,27 @@ import React from 'react'
 import styles from './Patentes.module.css'
 import Navabar from '../navbar/Navbar'
 import Footer from '../footer/Footer'
+import Axios from 'axios'
+import { useState, useEffect } from 'react'
 const Patentes = () => {
-  const APIURL = 'http://localhost:3001/patentes'
+  const [datePatentes, setDatePatentes] = useState ([])
+  useEffect (() => {
+    Axios.get ('http://localhost:3005/api/Patentes').then (res => {
+      setDatePatentes (res.data)
+      console.log(res.data)
+    })
+  }, [])
   return (
     <>
       <Navabar />
       <div className={styles.container}>
         <div className={styles.containerPatentes}>
           <h1 className={styles.title}>Patentes</h1>
+          <div className={styles.containerPatentes}>
+           
+          </div>
+
+
         </div>
       </div>
       <Footer />
